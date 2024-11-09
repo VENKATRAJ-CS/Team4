f46b0509c80a68a4ad75f2e51ac9db91b73af176
VENKATRAJ-CS <2222ka58@gmail.com>
2024-11-09 19:45:33 +0530

    RoomBooking.java

--- Team-4-Project/Hotel Room Booking System/Project/src/RoomBooking.java
+++ Team-4-Project/Hotel Room Booking System/Project/src/RoomBooking.java
@@ -16,7 +16,6 @@ public class RoomBooking {
             System.out.println("Bringing available Non-AC rooms...");
         }
 
-        // Example: available rooms
         int[] availableRooms = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println("Available rooms:");
@@ -29,19 +28,17 @@ public class RoomBooking {
         int roomNo = sc.nextInt();
         sc.nextLine();
 
-        // Assuming room is booked successfully
         cd.setBookedRoomNo(roomNo);
         System.out.println("Room no: " + roomNo + " selected successfully.");
         System.out.println("Room booking completed for " + cd.getName() + ". Enjoy your stay!");
 
-        // Exit after booking
         System.out.println("Would you like to vacate any room?");
         System.out.println("1) Yes");
         System.out.println("2) No");
         int vacateOption = sc.nextInt();
         sc.nextLine();
         if (vacateOption == 1) {
-            // Handle vacating room logic here if needed
+            
         } else {
             System.out.println("Thank you for your booking. Exiting the application.");
         }
